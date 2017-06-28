a59602d9ce75244c4332c2b48e3ec99504cf1498
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-06-28 01:17:15 -0400

    Updated comments for  update-scorecard-totals.js

--- update-scorecard-totals.js
+++ update-scorecard-totals.js
@@ -1,7 +1,31 @@
+// This webtask is called from a graphcool server side subscription with an event in the
+//shape of:
+// {
+//   "data": {
+//     "Score": {
+//       "updatedFields": [
+//         "updatedAt"
+//       ],
+//       "node": {
+//         "id": "cj4gj76xx00263k67blnld42q",
+//         "value": 18680,
+//         "scorecard": {
+//           "id": "cj4gj76xx00273k67n5si4vgt",
+//           "total": 9766,
+//           "communityAggregate": {
+//             "id": "cj4gj76xx00283k67hrlehdgm",
+//             "scoreTotal": 27713
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+//This function updates a user's Scorecard as well as the community aggregate score in response to a new score event 
+
 const request = require('request')
 
 module.exports = (context, cb) => {
-
   const node = context.body.data.Score.node
   const newPoints = node.value
   const previousScorecardTotal = node.scorecard.total
