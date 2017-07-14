39a8e2f2d4b978c2c210019802c26f7493afcbce
Patrick Strzelec <PatrickStrz@users.noreply.github.com>
2017-07-14 17:01:52 -0400

    Removed community aggregate update and changed api uri

--- update-scorecard-totals.js
+++ update-scorecard-totals.js
@@ -12,10 +12,6 @@
 //         "scorecard": {
 //           "id": "cj4gj76xx00273k67n5si4vgt",
 //           "total": 9766,
-//           "communityAggregate": {
-//             "id": "cj4gj76xx00283k67hrlehdgm",
-//             "scoreTotal": 27713
-//           }
 //         }
 //       }
 //     }
@@ -29,21 +25,16 @@ module.exports = (context, cb) => {
   const node = context.body.data.Score.node
   const newPoints = node.value
   const previousScorecardTotal = node.scorecard.total
-  const previousCommunityAggregateTotal = node.scorecard.communityAggregate.scoreTotal
 
   //To be used in mutation:
   const userScorecardId = node.scorecard.id
   const newScorecardTotal = previousScorecardTotal + newPoints
-  const communityAggregateId = node.scorecard.communityAggregate.id
-  const newCommunityAggregateTotal = previousCommunityAggregateTotal + newPoints
 
   console.log('newpoints:'+ newPoints)
   console.log('previousScorecardTotal:'+ previousScorecardTotal)
-  console.log('previousCommunityAggregateTotal:'+ previousCommunityAggregateTotal)
   console.log('-------------------------------------------------------------------')
 
-  const endpoint = 'https://api.graph.cool/simple/v1/cj2hsn8pvak4o0187k52n2i3l'
-  // const token = 'Bearer __PERMANENT_AUTH_TOKEN__'
+  const endpoint = 'https://api.graph.cool/simple/v1/cj541g35wjwqc01754kb4rfvk'
 
   const mutation = `
     mutation {
@@ -51,10 +42,6 @@ module.exports = (context, cb) => {
         id
         total
       }
-      updateCommunityAggregate(id:"${communityAggregateId}",scoreTotal:${newCommunityAggregateTotal}){
-        id
-        scoreTotal
-        }
     }`
     
    request.post({
